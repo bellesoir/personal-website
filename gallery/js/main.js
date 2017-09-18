0ad9c33f24e27060a6eb291362acf8a17c913174
Valerie Berrios <vberrios314@gmail.com>
2017-09-18 15:38:25 -0400

    Updated Gallery

--- gallery/js/main.js
+++ gallery/js/main.js
@@ -1,15 +1,15 @@
 //alert('main.js is linked');
 
-//reveal() will show/hide the caption for each image
-function reveal(id) {
-        var caption = document.getElementById(id);
-        var captionStyle = window.getComputedStyle(caption);
-        switch (captionStyle.display) {
-            case 'none':
-                caption.style.display = 'block';
-                break;
-            case 'block':
-                caption.style.display = 'none';
-                break;
-        }
-}
+$(document).ready(function(){
+
+/*Toggles image captions*/
+$('img').on('mouseover', function(){
+    var i = $.inArray($(this)[0],$('img'));
+    $($('.caption')[i]).toggle();
+});
+
+$('.caption').on('mouseout', function(){
+    $(this).toggle();
+});
+
+}); //CLOSES ready()
