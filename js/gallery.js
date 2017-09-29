e751fbccd2260cc68b9ef603a3675c0230ac15f5
Valerie Berrios <vberrios314@gmail.com>
2017-09-28 20:05:40 -0400

    updated

--- js/gallery.js
+++ js/gallery.js
@@ -1,18 +1,18 @@
-//alert('main.js is linked');
 
 $(document).ready(function(){
 
 /*Toggles image captions*/
-$('img').on('mouseover', function(){
+$('img').on('mouseenter', function(){
     var i = $.inArray($(this)[0],$('img')) - 2;
     $($('.caption')[i]).show();
 });
 
-$('.caption').on('mouseout', function(){
+$('.caption').on('mouseleave', function(){
     $(this).hide();
 });
 
 }); //CLOSES ready()
+
 /*
 $(window).on('resize', function(){
     $('.col').css('height', $('.col').css('width'));
